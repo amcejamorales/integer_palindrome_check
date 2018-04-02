83bc083f7b7a956eda986b3e4c87877555056da5
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-01 23:45:34 -0700

    Write functionality for is_palindrome method

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,40 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
+def find_length(number)
+
+  length = 0
+  dividend = 1
+
+  while number / dividend != 0
+    length += 1
+    dividend *= 10
+  end
+
+  return length
+end
+
+
 def is_palindrome(number)
-  raise NotImplementedError
+
+  return false if number.nil?
+  return false if number < 0
+
+  length = find_length(number)
+
+  divisor = 10 ** length
+  iteration = 0
+
+  while iteration < length / 2
+
+    top = ( number / ( divisor / 10) ) - ( ( number / divisor ) * 10 )
+    bottom = number - (( number / 10 ) * 10 )
+
+    return false if top != bottom
+
+    iteration += 1
+    divisor /= 100
+    number /= 10
+  end
+
+  return true
+
 end
